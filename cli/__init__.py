852abf9c82a7a1c6492b7894014a058d67201fd4
manohar3000 <manohargehlot3000@gmail.com>
2025-07-25 12:47:45 +0530

    deleted whole project

--- cli/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-CLI package for Murlix.
-"""
-from .session import SessionManager
-from .slash_commands import SlashCommandHandler
-from .chat_loop import run_chat_loop
-
-__all__ = ['SessionManager', 'SlashCommandHandler', 'run_chat_loop'] 
